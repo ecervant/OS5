dfa4a044f7ef9ed53ec95e3aa4aabea6bcfea3c5
Jessica Cioffi <jcioffi@nd.edu>
2017-04-06 18:28:49 -0400

    so it begins

--- main.c
+++ main.c
@@ -9,19 +9,29 @@ how to use the page table and disk interfaces.
 #include "page_table.h"
 #include "disk.h"
 #include "program.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
+// Global Variables
+const char *alg = argv[3];
+
+// Functions
 void page_fault_handler( struct page_table *pt, int page )
 {
-//    page_table_set_entry(pt,page,page,PROT_READ|PROT_WRITE);
-	printf("page fault on page #%d\n",page);
-	exit(1);
+  page_table_set_entry(pt,page,page,PROT_READ|PROT_WRITE);
+	
+  // write functions that performs a linear search to check if the physical memory is empty
+  // write a map thingy for frame, bits, dirty bit, & page
+  // if an item has a dirty bit, we have to update the disk before overwriting it
+  // 
+  
+  //printf("page fault on page #%d\n",page);
+	//exit(1);
 }
 
+// Main
 int main( int argc, char *argv[] )
 {
 	if(argc!=5) {
@@ -31,14 +41,14 @@ int main( int argc, char *argv[] )
 
 	int npages = atoi(argv[1]);
 	int nframes = atoi(argv[2]);
-	const char *program = argv[4];
+  const char *program = argv[4];
+  alg = argv[3];
 
 	struct disk *disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
 		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
 		return 1;
 	}
-
 
 	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
 	if(!pt) {
@@ -47,8 +57,21 @@ int main( int argc, char *argv[] )
 	}
 
 	char *virtmem = page_table_get_virtmem(pt);
-
 	char *physmem = page_table_get_physmem(pt);
+	
+  /*if(!strcmp(alg,"rand")) {
+		rand_program(virtmem,npages*PAGE_SIZE);
+
+	} else if(!strcmp(alg,"fifo")) {
+		fifo_program(virtmem,npages*PAGE_SIZE);
+
+	} else if(!strcmp(alg,"custom")) {
+		custom_program(virtmem,npages*PAGE_SIZE);
+
+	} else {
+		fprintf(stderr,"unknown algorithm: %s\n",argv[3]);
+		return 1;
+	}*/
 
 	if(!strcmp(program,"sort")) {
 		sort_program(virtmem,npages*PAGE_SIZE);
@@ -60,7 +83,7 @@ int main( int argc, char *argv[] )
 		focus_program(virtmem,npages*PAGE_SIZE);
 
 	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
+		fprintf(stderr,"unknown program: %s\n",argv[4]);
 		return 1;
 	}
 
